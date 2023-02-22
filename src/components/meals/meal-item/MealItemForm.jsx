76eee8144fb856bd3279f6f3d748f8caa97586c6
mirlnovna <aidaimakhmatova@gmail.com>
2023-02-22 16:05:10 +0600

    toolkit snackbar

---
+++ src/components/meals/meal-item/MealItemForm.jsx
@@ -0,0 +1,101 @@
+import React from "react";
+import styled from "styled-components";
+import Button from "../../UI/Button";
+import { ReactComponent as PlusIcon } from "../../../assets/icons/plus.svg";
+import { useState } from "react";
+import { addToBasket } from "../../../store/basket/basketSlice";
+import { useDispatch } from "react-redux";
+import { TextField } from "@mui/material";
+import { style } from "@mui/system";
+
+const MealItemForm = ({ id, title, price }) => {
+  const [amount, setAmount] = useState(1);
+
+  const dispatch = useDispatch();
+
+  const amountChangeHandler = (e) => {
+    setAmount(e.target.value);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const basketItem = {
+      id,
+      price,
+      title,
+      amount,
+    };
+
+    dispatch(addToBasket(basketItem));
+  };
+
+  return (
+    <StyledForm onSubmit={submitHandler}>
+      <Container>
+        <StyledLabel htmlFor={id}>Amount</StyledLabel>
+        <StyledInput
+          value={amount}
+          onChange={amountChangeHandler}
+          max={5}
+          min={0}
+          type="number"
+          id={id}
+        />
+      </Container>
+      <Container>
+        <TextField
+          id="outlined-number"
+          type="number"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          size="small"
+          autoFocus
+          error
+          multiline
+          // sx={{width:'70px',padding:'10px'}}
+        />
+      </Container>
+      <Button>
+        <PlusIcon />
+        Add
+      </Button>
+    </StyledForm>
+  );
+};
+
+export default MealItemForm;
+
+const StyledTextField = style(TextField)(() => ({
+  root: {
+    width: "70px",
+  },
+}));
+
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+`;
+
+const Container = styled.div`
+  margin-bottom: 15px;
+`;
+
+const StyledLabel = styled.label`
+  font-weight: 600;
+  font-size: 1.125rem;
+  line-height: 1.6875rem;
+  margin: 0 1.25rem 0 0;
+`;
+
+const StyledInput = styled.input`
+  width: 3.75rem;
+  height: 2rem;
+  outline: none;
+  border-radius: 6px;
+  border: 1px solid #d6d6d6;
+  font-weight: 500;
+  font-size: 16px;
+`;
